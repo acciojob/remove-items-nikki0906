3710ea8e9f772b783fe1740d699cd3a047fbbbf1
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-06-01 10:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+            const selectElement = document.getElementById('colorSelect');
+            selectElement.remove(selectElement.selectedIndex);
+}
